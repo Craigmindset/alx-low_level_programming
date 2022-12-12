d95dda88405cff1273ea13a6fdda4e34a28d38ac
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2022-12-12 09:36:13 +0100

    purchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 /**
- *main  - Entry Point
- *Return     Alaways return upon success
- * Description: if else statement
- *Betty style doc implemented
+ * main	-	Entry point
+ * Return:	Always return 0 (success)
+ * Description: If else statement
+ * betty style doc for function main goes there
  */
-int main (void)
+int main(void)
 {
+	char c;
 
-  char c;
-
-  for (c = 'a'; c <= 'z'; ++c)
-    {
-     if (c == 'e')
-	continue;
-      else if (c  == 'q')
-	continue;
-      else
-
-	putchar(c);
-    }
-  putchar(10);
-  return (0);
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == 'e')
+			continue;
+		else if (c == 'q')
+			continue;
+		else
+			putchar(c);
+	}
+	putchar(10);
+	return (0);
 }
