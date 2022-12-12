a3859b87e16c5418812ec17852bffc532d77e520
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2022-12-12 02:07:15 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,47 +2,25 @@
 #include <stdlib.h>
 
 /**
-
  * main - returns lowercase and uppercase alphabets
-
  *
-
  * Return: Always 0
-
  */
-
 int main(void)
-
 {
+	int alph = 'a';
+	int ALPH = 'A';
 
-  int alph = 'a';
-
-  int ALPH = 'A';
-
-
-
-  while (alph <= 'z')
-
-    {
-
-      putchar(alph);
-
-      alph++;
-
-    }
-
-  while (ALPH <= 'Z')
-
-    {
-
-      putchar(ALPH);
-
-      ALPH++;
-
-    }
-
-  putchar('\n');
-
-  return (0);
-
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	while (ALPH <= 'Z')
+	{
+		putchar(ALPH);
+		ALPH++;
+	}
+	putchar('\n');
+		return (0);
 }
