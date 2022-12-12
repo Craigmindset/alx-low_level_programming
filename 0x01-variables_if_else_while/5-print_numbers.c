bb09f10640f9c6dd7ced64f3a6f22d723531158d
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2022-12-12 09:38:31 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main	-	Entry point
+ * Return:	Always return 0 (success)
+ * Description: If else statement
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar(10);
+	return (0);
+}
