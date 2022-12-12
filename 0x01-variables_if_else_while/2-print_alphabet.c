9e934014cab7fbcea02d5f271d678b17c5919053
Craigmindset <irediacraig@gmail.com>
2022-12-11 16:43:25 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,23 +7,23 @@
  * Return: Always 0
 */
 int main(void)
-  
+
 {
-  
+
   int alph = 'a';
-  
+
   while (alph <= 'z')
-    
+
     {
-      
+
       putchar(alph);
-      
+
       alph++;
-      
+
     }
-  
+
   putchar('\n');
-  
+
   return (0);
 
 }
