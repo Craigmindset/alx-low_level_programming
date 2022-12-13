61f73fb3ba3a7a745c2fa8dc208f94600bf76ddb
Craigmindset <irediacraig@gmail.com>
2022-12-13 08:07:47 -0800

    update

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main()
+  
+{
+  
+  char* i;
+  
+  for(i='a'; i<='z'; i++)
+    
+    {
+      
+      putchar(i);
+      
+    }
+  
+  putchar('\n');
+  
+
+  
+  return 0;
+  
+}
