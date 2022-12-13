9d5666f9ffbd04cbfaa54829012443f63dcbabe8
Craigmindset <irediacraig@gmail.com>
2022-12-13 14:42:02 -0800

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - prints putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('/n');
+
+  return (0);
+}
