0c0b1e754d568f1d150c921e37736e6e4d61d5cd
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2022-12-13 22:17:52 +0100

    print lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
-int main()
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
 {
-  char* i;
-  for(i='a'; i<='z'; i++)
-    {
-      putchar(i);
-    }
-  putchar('\n');
+	char alph = 'a';
 
-  return (0);
-
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
 }
