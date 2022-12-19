3efd8d37e07f1c013c94e20196cc2ecf5c043fd8
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2022-12-20 00:06:26 +0100

    Main.h lib

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _putchar(char);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -12,4 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+
 #endif
+
