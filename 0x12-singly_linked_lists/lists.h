58e1c684437a75f56873e79e15b614e533212e15
craigmindset <117746620+Craigmindset@users.noreply.github.com>
2023-01-27 00:14:41 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef _LISTS_
 #define _LISTS_
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -13,16 +15,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-typedef struct list_s
-{
-  char *str;
-  unsigned int len;
-  struct list_s *next;
-} list_t
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
