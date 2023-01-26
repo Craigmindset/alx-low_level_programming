ae456dc7e164d2a8051d0c27dc779398d68de1da
Craigmindset <irediacraig@gmail.com>
2023-01-26 13:11:01 -0800

    readme updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,6 +14,12 @@
  * Description: singly linked list node structure
  * for alx cohort 11 project
  */
+typedef struct list_s
+{
+  char *str;
+  unsigned int len;
+  struct list_s *next;
+} list_t
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
